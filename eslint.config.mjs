29c4a2c2f7de29fb04797c7ec4e590dac05e72ee
Sebastian Cerviño <cervisebas@gmail.com>
2025-10-05 14:52:08 -0300

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,46 @@
+import { defineConfig } from 'eslint/config';
+import tseslint from '@electron-toolkit/eslint-config-ts';
+import eslintConfigPrettier from '@electron-toolkit/eslint-config-prettier';
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
+import eslintPluginReactRefresh from 'eslint-plugin-react-refresh';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default defineConfig(
+  { ignores: ['**/node_modules', '**/dist', '**/out'] },
+  tseslint.configs.recommended,
+  eslintPluginReact.configs.flat.recommended,
+  eslintPluginReact.configs.flat['jsx-runtime'],
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      'react-hooks': eslintPluginReactHooks,
+      'react-refresh': eslintPluginReactRefresh,
+    },
+    rules: {
+      // 1. Comillas simples obligatorias
+      quotes: ['error', 'single', { avoidEscape: true }],
+
+      // 2. Línea en blanco al final del archivo
+      'eol-last': ['error', 'always'],
+
+      // 3. Coma final obligatoria
+      'comma-dangle': ['error', 'always-multiline'],
+
+      // 4. Punto y coma obligatorio
+      semi: ['error', 'always'],
+
+      ...eslintPluginReactHooks.configs.recommended.rules,
+      ...eslintPluginReactRefresh.configs.vite.rules,
+    },
+  },
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
+);
