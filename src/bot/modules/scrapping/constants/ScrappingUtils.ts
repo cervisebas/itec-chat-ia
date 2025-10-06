585378fca387dc28ea995cf6dd3ae51d0f6e334c
Sebastian Cerviño <cervisebas@gmail.com>
2025-10-06 00:57:20 -0300

    fix: se corrigio un error de build

--- src/bot/modules/scrapping/constants/ScrappingUtils.ts
+++
@@ -1,6 +0,0 @@
-export const ScrappingUtils = {
-  SEPARATOR: Array(160).fill('-').join(''),
-  REFERENCE(link: string, content: string) {
-    return `Enlace de referencia: ${link}\n\n${content}`;
-  },
-};
--- src/bot/modules/scrapping/interfaces/ScrappingExtract.ts
+++
@@ -1,4 +0,0 @@
-export interface ScrappingExtract {
-  extract(): Promise<string>;
-  getName(): string;
-}
